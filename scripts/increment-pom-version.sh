ce6942bdb5a45a37c6d380f49fa837fd4a86ca0a
Jack Weaver <jack.weaver@sadasystems.com>
2017-05-20 15:01:09 -0700

    Setup publish and maven version scripts
    Setup a publish script placeholder.  This can be extended to do a whole
    lot more.  Also introduce a basic maven versioning (and git tagging)
    script for all maven based CI builds.

--- scripts/increment-pom-version.sh
+++ scripts/increment-pom-version.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 #
-# TODO
+# Increment the version in our maven project.  This mimics the maven
+# release plugin to some degree.
+#
+# Tag current commit we are on with the version that's in the pom
+# file.  Bump that version and commit, then finally push it all back
+# to github.
+set -o errexit -o errtrace -o nounset
+
+
+# TODO: Set up some dependency checks: Ensure maven is present, ensure
+# ${CI} is present, ensure pom file exists, etc.
+
+
+# Capture Current version & tag it.
+declare CURRENT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+git tag -a v$CURRENT_VERSION -m "v$CURRENT_VERSION"
+
+# Set next version and bump pom file to it.
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit;
+declare NEW_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+
+echo "  - Commit to github.."
+git add pom.xml
+# Including "[skip ci]" in the commit message ensures we don't trigger
+# another CI build on master.
+git commit -am "Set next release version:  $NEW_VERSION
+
+    Source of this Commit: https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}
+    [skip ci]"
+git push origin HEAD:master
+git push origin --tags
+echo "  - Success.  Done."
--- scripts/publish-docker-image.sh
+++ scripts/publish-docker-image.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 #
-# TODO
+# Publish a docker image that was built.  This lets us easily push
+# built docker images from CI jobs into Artifactory.
+set -o errexit -o errtrace -o nounset
+
+# TODO: Check environment, fail if ${CI} isn't present, etc.  Also
+# take better care of the arguments passed to this script.
+
+# TODO: Beyond publishing, we can extend this script to do more as
+# well.  Typically if it's just 1 gradle jar or war in a docker image,
+# we can make this script build the docker image as well (using the
+# same version as the java artifact).
+
+docker push meremortal-mortal-docker.jfrog.io/${1}:${2}
